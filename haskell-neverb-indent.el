4d17f685c72a0ed71ec505382fc82f3cefd73cbf
Alexander Tumin <itakingiteasy@gmail.com>
2011-07-03 01:43:33 +0400

    Regressions

--- haskell-neverb-indent.el
+++ haskell-neverb-indent.el
@@ -43,16 +43,37 @@
 			  (progn
 				(setq wasidented t)
 				(setq left-margin eqmatch))))))
+
+  (setq rexp "^[[:space:]]*\\(|\\)")
+  (if (not wasidented)
+	  (progn
+		(string-match rexp currline)
+		(if (match-end 1)
+			(progn
+			  (string-match rexp prevline)
+			  (if (not (match-end 1))
+				  (progn
+					(print "lol")
+					(setq wasidented t)
+					(setq left-margin (+ shiftwidth prevmuch))
+				   )
+			   )
+			  )
+		  )
+		)
+	)
   
 
   (if (not wasidented)
 	  (progn
-		(string-match "^[[:space:]]*\\(where\\|do\\|let\\|if\\|case\\)\\([[:space:]]\+\\).*" currline)
-		(let ((eqmatch (match-end 2)))
-		(if eqmatch
+		(string-match "^[[:space:]]*\\(where\\|do\\|let\\|if\\|case\\)" currline)
+		(let ((eqmatch  (match-beginning 0)))
+		(if (/= 0 eqmatch)
 			  (progn
+				(print "kaka")
 				(setq wasidented t)
-				(setq left-margin (+ shiftwidth prevmuch)))))))
+				(setq left-margin (+ shiftwidth prevmuch))
+				)))))
 
 
 
@@ -70,12 +91,15 @@
   
   (if (not wasidented)
 	  (progn
-		(string-match "\\([!#$%&*+./<=>?@\\^|~-]$\\|[[:space:]]do$\\)" prevline)
-		(let ((eqmatch (match-beginning 1)))
-		  (if eqmatch
+		(string-match "\\([!#$%&*+./<=>?@\\^|~-]\\|[[:space:]]do\\)$" prevline)
+		(let ((eqmatch (match-beginning 0)))
+		  (if (not eqmatch)
 			  (progn
 				(setq wasidented t)
-				(setq left-margin (+ shiftwidth prevmuch)))))))
+				(setq left-margin (+ shiftwidth prevmuch))
+				)))))
+
+  
   (if (not wasidented)
 	  (progn
 		(string-match "^[[:space:]]*data[^=]*\\(=\\).*$" prevline)
@@ -88,26 +112,38 @@
   
 
   (string-match "^[[:space:]]*$" prevline)
-  (if (and (not wasidented) (/= 0 (match-beginning 0)))
-	  (progn
-		(string-match "^[^[:space:]]\+" (thing-at-point 'line))
-		(if (/= 0 (match-beginning 0))
+  (if (not wasidented)
+	  (if (/= 0 (match-beginning 0))
+;;	  (progn
+;;		(string-match "^[^[:space:]]\+" (thing-at-point 'line))
+;;		(if (/= 0 (match-beginning 0))
+		  (progn
+			(setq wasidented t)
+			(print "zzz")
 			(setq left-margin prevmuch)
-		  (setq left-margin (+ prevmuch shiftwidth))
-		  )))
+			)
+		)
+;;		  (setq left-margin (+ prevmuch shiftwidth))
+	);))
 
   
-    
-  (indent-to-left-margin)
   
+    
   (set-match-data mdata)
 )
 
+(defun haskell-neverb-indent-del ()
+  (interactive)
+  (delete-backward-char 1)
+  ;;(indent-to-left-margin)
+;;  (message (number-to-string left-margin))
+  )
 
 (defun haskell-neverb-indent ()
   (interactive)
   (haskell-neverb-indent-line)
-  )
+  (indent-to-left-margin)
+    )
 
 
 (defun turn-on-haskell-neverb-indent ()
@@ -115,4 +151,6 @@
   (run-hooks 'haskell-neverb-indent-hook))
 
 (provide 'haskell-neverb-indent)
-
+(provide 'haskell-neverb-indent-del)
+  
+  
